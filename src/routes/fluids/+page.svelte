330b6135b1730c86e64030b55dc8fcf48084dbc8
dave morgan <dave@dmorg.org>
2022-11-21 23:46:37 -0700

    backtrack

---
+++ src/routes/fluids/+page.svelte
@@ -0,0 +1,5 @@
+<h3>it's getting wet in here</h3>
+
+<a href="/">Home is where the heart is</a>
+<p></p>
+<a href="/fluids/12OpenChannel">Open Channel</a>
--- src/routes/fluids/12OpenChannel/+page.svelte
+++ src/routes/fluids/12OpenChannel/+page.svelte
@@ -1,3 +1,5 @@
-fluids 
+<h1>open channel</h1>
 
-!$a^!$
+$$a^2=b^2+c^2$$
+
+<a href="/">Back home, Guido</a>
