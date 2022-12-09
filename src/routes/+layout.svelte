984ffe25da468d1eb1bf5d23cf485c90cd344aa6
dave morgan <dave@dmorg.org>
2022-12-08 17:17:38 -0700

    more katesting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,14 @@
 <script>
-	import { onMount, afterUpdate } from "svelte";
-	import "$lib/styles/app.css";
-	let katexify;
+    import MathJax from "mathjax";
 
-	onMount(() => {
-		 katexify = () => {
-			renderMathInElement(document.body, {
-				delimiters: [
-					{
-						left: "$$",
-						right: "$$",
-						display: true,
-					},
-					{
-						left: "!$",
-						right: "!$",
-						display: false,
-					},
-				],
-			});
-		};
-		katexify();
-	});
-	// might only need this during development?
-	afterUpdate(() => {
-		katexify();
-		katexify();
-	});
+    MathJax = {
+        tex: {
+            inlineMath: [
+                ["$", "$"],
+                ["\\(", "\\)"],
+            ],
+        },
+    };
 </script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.css"
-		integrity="sha384-Juol1FqnotbkyZUT5Z7gUPjQ9gzlwCENvUZTpQBAPxtusdwFLRy382PSDx5UUJ4/"
-		crossorigin="anonymous"
-	/>
-
-	<!-- The loading of KaTeX is deferred to speed up page rendering -->
-	<script
-		defer
-		src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.js"
-		integrity="sha384-97gW6UIJxnlKemYavrqDHSX3SiygeOwIZhwyOKRfSaf0JWKRVj9hLASHgFTzT+0O"
-		crossorigin="anonymous"
-	></script>
-
-	<!-- To automatically render math in text elements, include the auto-render extension: -->
-	<script
-		defer
-		src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/contrib/auto-render.min.js"
-		integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05"
-		crossorigin="anonymous"
-		onload="renderMathInElement(document.body);"
-	></script>
-</svelte:head>
-
-<slot />
+<slot/>
--- src/routes/fluids/12OpenChannel/+page.svelte
+++ src/routes/fluids/12OpenChannel/+page.svelte
@@ -1,5 +1,5 @@
 <h1>open channel</h1>
 
-<!-- $$a^2=b^2+c^2$$ -->
+$$a^2=b^2+c^2$$
 
 <a href="/">Back home, Guido</a>
